642ca107f303e012a98594dd2289558c43f99744
Alyxe Lett <alyxe@scripted.org>
2018-04-13 16:48:16 +0000

    We made our website!

--- js/script.js
+++ js/script.js
@@ -2,7 +2,41 @@
 
 console.log('hi');
 
-$('h1').click(function(){
-    $('h1').css('color', 'green');
-    $('h1').text('Ready to Code');
+
+
+$('button').click(function(){
+    var userInput = $('input').val();
+    console.log(userInput);
+
+
+    $.ajax({
+        url:"https://www.omdbapi.com/?apikey=90d4b10a&y=2016&s=" + userInput,
+        method: "GET",
+        success: function(response){
+            console.log(response);
+            displayTitle(response);
+            //displayYear(response);
+            imdbLINK(response);
+        },
+    });
+
 });
+
+function displayTitle(response){
+    console.log(response.Search[0].Title);
+    $('#name').append(response.Search[0].Title);
+}
+
+function displayYear(response){
+    console.log(response);
+    var imdbResponse = response.Search[0].imdbID;
+    console.log(imdbResponse);
+    $('#year').append("<a href='imdb.com/title/" + imdbResponse + "> Check it out here! </a>");
+}
+
+function imdbLINK(response){
+    console.log(response);
+    var imdbResponse = response.Search[0].imdbID;
+    console.log(imdbResponse);
+    $('#description').append("<a href='https://imdb.com/title/" + imdbResponse + "'> Check it out here! </a>");
+}
